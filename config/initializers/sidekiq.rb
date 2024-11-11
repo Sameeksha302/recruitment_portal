df89f2a1ac7b1554d3c8c1071ad5a1d3896b8f3c
Sameeksha Singh <96697486+Sameeksha302@users.noreply.github.com>
2024-11-11 15:20:06 +0530

    Update sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -11,29 +11,29 @@
 # config.redis = { url: "redis://localhost:6379/0" }
 # end
 
+require "sidekiq-scheduler"
+Sidekiq.configure_server do |config|
+    config.redis = {
+      url: Rails.env.development? ? "redis://localhost:6379/0" : "redis://red-csm94artq21c738ec9d0:6379"
+    }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = {
+      url: Rails.env.development? ? "redis://localhost:6379/0" : "redis://red-csm94artq21c738ec9d0:6379"
+    }
+  end
+
+
+# require "sidekiq-scheduler"
 
 # Sidekiq.configure_server do |config|
-#     config.redis = {
-#       url: Rails.env.development? ? "redis://localhost:6379/0" : "redis://red-csm94artq21c738ec9d0:6379"
-#     }
-#   end
+#     config.redis = { url: ENV["REDIS_URL"]}
+#     config.on(:startup) do
+#         Sidekiq::Scheduler.load_schedule!
+#     end
+# end
 
-#   Sidekiq.configure_client do |config|
-#     config.redis = {
-#       url: Rails.env.development? ? "redis://localhost:6379/0" : "redis://red-csm94artq21c738ec9d0:6379"
-#     }
-#   end
-
-
-require "sidekiq-scheduler"
-
-Sidekiq.configure_server do |config|
-    config.redis = { url: ENV["REDIS_URL"]}
-    config.on(:startup) do
-        Sidekiq::Scheduler.load_schedule!
-    end
-end
-
-Sidekiq.configure_client do |config|
-config.redis = { url: ENV["REDIS_URL"]}
-end
+# Sidekiq.configure_client do |config|
+# config.redis = { url: ENV["REDIS_URL"]}
+# end
