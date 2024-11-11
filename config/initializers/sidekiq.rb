58d30e000bae30cf76d9a2434733b06ca0f7c4c5
Sameeksha Singh <96697486+Sameeksha302@users.noreply.github.com>
2024-11-11 15:27:31 +0530

    Update sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -14,13 +14,13 @@
 require "sidekiq-scheduler"
 Sidekiq.configure_server do |config|
     config.redis = {
-      url: Rails.env.development? ? "redis://localhost:6379/0" : "redis://red-csm94artq21c738ec9d0:6379"
+      url: Rails.env.development? ? "redis://localhost:6379/0" : ENV["REDIS_URL"]
     }
   end
 
   Sidekiq.configure_client do |config|
     config.redis = {
-      url: Rails.env.development? ? "redis://localhost:6379/0" : "redis://red-csm94artq21c738ec9d0:6379"
+      url: Rails.env.development? ? "redis://localhost:6379/0" : ENV["REDIS_URL"]
     }
   end
 
